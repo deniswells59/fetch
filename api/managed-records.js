b4785ebe27ebe4b02f6f5587f0af0da93ca0bbaf
Denis Wells <deniswells59@gmail.com>
2018-01-25 07:44:45 -0800

    formatted

--- api/managed-records.js
+++ api/managed-records.js
@@ -7,46 +7,82 @@ window.path = "http://localhost:3000/records";
 const primaryColors = ['red', 'blue', 'yellow'];
 const defaultOpts   = { page: 1, colors: [] };
 
+/**
+   * Retrieves records from API, uses options object to build
+   * query string.
+   * @param {Object} options - options to query for
+   * @param {string} options.page - the page to query for
+   * @param {string[]} options.colors - the colors to query for
+   * @returns {Promise} Promise object represents API response
+   */
 const retrieve = options => {
-  return new Promise((resolve, reject) => {
+  return new Promise(resolve => {
+
+    // Defaults if missing data
     if(!options) options = defaultOpts;
     if(!options.page) options.page = defaultOpts.page;
 
-    let url = buildUrl(options);
+    const url = buildUrl(options);
 
     fetch(url)
+      .then(res => res.json())
       .then(res => {
-        return res.json()
-      })
-      .then(res => {
-        let responseObj = buildResponse(res, options);
+        const responseObj = buildResponse(res, options);
         return resolve(responseObj);
       })
       .catch(err => {
         console.log('ERR', err);
-        return resolve({})
+        return resolve({});
       });
 
   });
 }
 
-const buildResponse = (dataArr, options) => {
-  let nextPage = dataArr.splice(10).length > 0;
+/**
+   * Builds URL for based on options used.
+   * @param {Object} opts - options to query for
+   * @returns {string} url and query string
+   */
+const buildUrl = opts => {
+  const url = new URI(window.path);
+  if(!opts) return url.toString();
 
-  let ids  = dataArr.map(returnIds);
-  let open = dataArr
+  const urlOpts = {
+    limit: 11,
+    offset: (opts.page - 1) * 10,
+    'color[]': opts.colors
+  };
+
+  url.query(urlOpts);
+
+  return url.toString();
+}
+
+/**
+   * Builds specific response object to resolve request.
+   * @param {Object[]} dataArr - response from API
+   * @param {Object} options - options to query for
+   * @returns {Object} specific response object
+   */
+const buildResponse = (dataArr, options) => {
+  // Boolean checks for next pages,
+  // splice to removes extra record.
+  const nextPageExists = dataArr.splice(10).length > 0;
+
+  const ids  = dataArr.map(returnIds);
+  const open = dataArr
               .filter(r => returnByDisposition(r, 'open'))
               .map(addPrimaryBool);
 
-  let closedPrimaryCount = dataArr
+  const closedPrimaryCount = dataArr
                             .filter(r => returnByDisposition(r, 'closed'))
                             .filter(returnPrimary)
                             .length;
 
-  let previousPage = options.page > 1 ? options.page - 1 : null;
-  let nextPage     = nextPage ? options.page + 1 : null;
+  const previousPage = options.page > 1 ? options.page - 1 : null;
+  const nextPage     = nextPageExists ? options.page + 1 : null;
 
-  let responseObj = {
+  const responseObj = {
     ids,
     open,
     previousPage,
@@ -57,6 +93,9 @@ const buildResponse = (dataArr, options) => {
   return responseObj;
 }
 
+
+/////////////      Build Response Helpers     ///////////////////
+/////////////////////////////////////////////////////////////////
 const returnPrimary= record => {
   return primaryColors.indexOf(record.color) >= 0;
 }
@@ -73,21 +112,6 @@ const addPrimaryBool = record => {
   record.isPrimary = false;
   if(returnPrimary(record)) record.isPrimary = true;
   return record;
-}
-
-const buildUrl = opts => {
-  let url = new URI(window.path);
-  if(!opts) return url.toString();
-
-  let urlOpts = {
-    limit: 11,
-    offset: (opts.page - 1) * 10,
-    'color[]': opts.colors
-  };
-
-  url.query(urlOpts);
-
-  return url.toString();
 }
 
 export default retrieve;
